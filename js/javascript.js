cdc81493b695105d8811d48e23523b7a192e4872
Victor Treaba <vtreaba@gmail.com>
2017-10-06 21:34:59 -0400

    Test

--- js/javascript.js
+++ js/javascript.js
@@ -3,28 +3,29 @@ let curPage = 0;
 
 window.addEventListener('load',
     function () {
+        $(".welcomeName").fadeIn(3000, function () {
+            $(".welcomeFlair").fadeIn(3000, function () {
+                $(".welcome").fadeOut("slow", function () {
+                    (function () {
+                        let redirect = sessionStorage.redirect;
+                        delete sessionStorage.redirect;
+                        if (redirect && redirect !== location.href) {
+                            let check = redirect.replace("http://victortreaba.com/", "");
+                            if (pages.indexOf(check) < 0) {
+                                check = "not";
+                                redirect = "404"
+                            }
+                            history.replaceState(null, "", redirect);
+                            document.getElementsByClassName(pages[curPage])[0].style.display = "none";
+                            document.getElementsByClassName(check)[0].style.display = "block";
 
-    $(".welcome").fadeIn(3000, function() {
-        $(".welcome").fadeOut("slow", function() {
-            (function () {
-                let redirect = sessionStorage.redirect;
-                delete sessionStorage.redirect;
-                if (redirect && redirect !== location.href) {
-                    let check = redirect.replace("http://victortreaba.com/", "");
-                    if (pages.indexOf(check) < 0) {
-                        check = "not";
-                        redirect = "404"
-                    }
-                    history.replaceState(null, "", redirect);
-                    document.getElementsByClassName(pages[curPage])[0].style.display = "none";
-                    document.getElementsByClassName(check)[0].style.display = "block";
-
-                    curPage = pages.indexOf(check);
-                } else {
-                    $(".home").fadeIn(2000, function () {
-                    });
-                }
-            })();
+                            curPage = pages.indexOf(check);
+                        } else {
+                            $(".home").fadeIn(2000, function () {
+                            });
+                        }
+                    })();
+                });
+            });
         });
-    });
     }, false);
